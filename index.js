273fe9eb285163b409882ff97b3035ad07ac0574
nvkvasiliy <vases74@gmail.com>
2022-01-29 02:53:11 +0500

    init

---
+++ index.js
@@ -0,0 +1,28 @@
+/** Класс кофе */
+class Coffee {
+  /**
+   * Создаёт экзмепляр кофе
+   * @param {string} coffeName - название кофе
+   * @param {number} preparationTime - время приготовления кофе
+   */
+  constructor(coffeName, preparationTime) {
+  }
+}
+
+/** Класс кофемашины */
+class CoffeeMachine {
+  /**
+   * Создаёт экзмепляр кофемашины
+   * @param {number} maxCup - кол-во чашек, в которые параллельно можно готовить кофе
+   */
+  constructor(maxCup) {
+  }
+
+  /**
+   * Запускает приготовление кофе
+   * @param {Coffee} coffee - кофе, которое требуется приготовить
+   * @returns {Promise<Coffee>} - промис, который выполнится после приготовленя кофе
+   */
+  createCoffee(coffee) {
+  }
+}
