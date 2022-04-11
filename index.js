a191417a47a97cb1802e6653815092d7b8e977cd
NikitaKirin <kirinnikita1406@gmail.com>
2022-04-11 23:21:56 +0500

    first done

--- index.js
+++ index.js
@@ -1,30 +1,68 @@
 /** Класс кофе */
+const {rejects} = require("assert");
+
 class Coffee {
-  /**
-   * Создаёт экзмепляр кофе
-   * @param {string} name - название кофе
-   * @param {number} preparationTime - время приготовления кофе
-   */
-  constructor(name, preparationTime) {
-  }
+    /**
+     * Создаёт экзмепляр кофе
+     * @param {string} name - название кофе
+     * @param {number} preparationTime - время приготовления кофе
+     */
+    constructor(name, preparationTime) {
+        if (typeof name !== 'string') {
+            throw new Error('Передано неверное имя!');
+        }
+        if (typeof preparationTime !== 'number') {
+            throw new Error('Передано неверное время приготовления!');
+        }
+        this.name = name;
+        this.preparationTime = preparationTime;
+    }
 }
 
 /** Класс кофемашины */
 class CoffeeMachine {
-  /**
-   * Создаёт экзмепляр кофемашины
-   * @param {number} maxCup - кол-во чашек, в которые параллельно можно готовить кофе
-   */
-  constructor(maxCup) {
-  }
+    /**
+     * Создаёт экземпляр кофемашины
+     * @param {number} maxCup - кол-во чашек, в которые параллельно можно готовить кофе
+     */
+    constructor(maxCup, wearLevel = 4) {
+        if (typeof maxCup !== 'number') {
+            throw new Error('Передано неверное количество чашек!');
+        }
+        if (typeof wearLevel !== 'number') {
+            throw new Error('Передано неверное состояние износа!');
+        }
+        this.maxCup = maxCup;
+        this.queueOrders = [];
+        this.wearLevel = wearLevel;
+    }
 
-  /**
-   * Запускает приготовление кофе
-   * @param {Coffee} coffee - кофе, которое требуется приготовить
-   * @returns {Promise<Coffee>} - промис, который выполнится после приготовления кофе
-   */
-  createCoffee(coffee) {
-  }
+    /**
+     * Запускает приготовление кофе
+     * @param {Coffee} coffee - кофе, которое требуется приготовить
+     * @returns {Promise<Coffee>} - промис, который выполнится после приготовления кофе
+     */
+    async createCoffee(coffee) {
+        if (!(coffee instanceof Coffee)) {
+            throw new Error('Передан не объект кофе!');
+        }
+        const promise = await new Promise(async (resolve, reject) => {
+            if (this.wearLevel > 0) {
+                this.wearLevel--;
+                if (this.queueOrders.length > this.maxCup) {
+                    await Promise.race(this.queueOrders.map((order) => order))
+                }
+                setTimeout(() => {
+                    this.queueOrders.shift()
+                    resolve(coffee);
+                }, coffee.preparationTime);
+            } else {
+                reject(coffee);
+            }
+        });
+        this.queueOrders.push(promise);
+        return promise;
+    }
 }
 
-module.exports = { Coffee, CoffeeMachine };
+module.exports = {Coffee, CoffeeMachine};
