916e6a65ff085ee719a0f73e898b8e4260aff179
Grekova Elena <grekovalena01@gmail.com>
2022-04-11 22:01:20 +0500

    Грекова Елена

--- index.js
+++ index.js
@@ -1,30 +1,67 @@
 /** Класс кофе */
 class Coffee {
-  /**
-   * Создаёт экзмепляр кофе
-   * @param {string} name - название кофе
-   * @param {number} preparationTime - время приготовления кофе
-   */
-  constructor(name, preparationTime) {
-  }
+    /**
+     * Создаёт экзмепляр кофе
+     * @param {string} name - название кофе
+     * @param {number} preparationTime - время приготовления кофе
+     */
+    constructor(name, preparationTime) {
+        this.name = name;
+        this.preparationTime = preparationTime;
+    }
 }
 
 /** Класс кофемашины */
 class CoffeeMachine {
-  /**
-   * Создаёт экзмепляр кофемашины
-   * @param {number} maxCup - кол-во чашек, в которые параллельно можно готовить кофе
-   */
-  constructor(maxCup) {
-  }
+    /**
+     * Создаёт экзмепляр кофемашины
+     * @param {number} maxCup - кол-во чашек, в которые параллельно можно готовить кофе
+     * @param {number} wearLevel - уровень износа кофемашины
+     */
+    constructor(maxCup, wearLevel) {
+        this.maxCup = maxCup;
+        this.wearLevel = 4;
+        this.queue = [];
+    }
 
-  /**
-   * Запускает приготовление кофе
-   * @param {Coffee} coffee - кофе, которое требуется приготовить
-   * @returns {Promise<Coffee>} - промис, который выполнится после приготовления кофе
-   */
-  createCoffee(coffee) {
-  }
+    /**
+     * Запускает приготовление кофе
+     * @param {Coffee} coffee - кофе, которое требуется приготовить
+     * @returns {Promise<Coffee>} - промис, который выполнится после приготовления кофе
+     */
+    createCoffee(coffee) {
+        if (!coffee instanceof Coffee) throw new Error();
+
+        let cupOfCoffee = new Promise(async (resolve, reject) => {
+            if (this.wearLevel < 1) {
+                reject(coffee);
+            }
+            if (this.queue.length < this.maxCup) {
+                setTimeout(() => {
+                        let deleteIndex = this.queue.findIndex(p => p === this);
+                        this.queue.slice(deleteIndex, 1);
+                        resolve(coffee);
+                    },
+                    coffee.preparationTime)
+            } else {
+                let preparingCoffee = [];
+                for (let i = 0; i < this.maxCup; i++) {
+                    preparingCoffee.push(this.queue[i]);
+                }
+                await Promise.race(preparingCoffee);
+                setTimeout(() => {
+                        let deleteIndex = this.queue.findIndex(p => p === this);
+                        this.queue.slice(deleteIndex, 1);
+                        resolve(coffee);
+                    },
+                    coffee.preparationTime)
+            }
+        });
+
+        this.queue.push(cupOfCoffee);
+        this.wearLevel -= 1;
+        return cupOfCoffee;
+    }
 }
 
-module.exports = { Coffee, CoffeeMachine };
+module.exports = {Coffee, CoffeeMachine};
