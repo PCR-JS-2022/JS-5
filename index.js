449110f831691d109497cec542cf36e98491d2ab
Tosikperm59 <tosikperm59@gmail.com>
2022-04-11 11:42:18 +0500

    Full version

--- index.js
+++ index.js
@@ -1,30 +1,74 @@
-/** Класс кофе */
+
 class Coffee {
-  /**
-   * Создаёт экзмепляр кофе
-   * @param {string} name - название кофе
-   * @param {number} preparationTime - время приготовления кофе
-   */
+
   constructor(name, preparationTime) {
+
+    if (!name === 'string' || !name)
+     throw new Error ('Не верный формат имени');
+
+    if (!preparationTime === 'number' || !preparationTime || preparationTime <= 0)
+     throw new Error ('Не верный формат времени');
+
+     this.name = name;
+     this.preparationTime = preparationTime;
   }
 }
 
-/** Класс кофемашины */
+
 class CoffeeMachine {
-  /**
-   * Создаёт экзмепляр кофемашины
-   * @param {number} maxCup - кол-во чашек, в которые параллельно можно готовить кофе
-   */
-  constructor(maxCup) {
+
+  constructor(maxCup, wearLevel = 4) {
+
+    if (!maxCup === 'number' || !maxCup)
+      throw new Error('Не верный формат данных');
+
+    this.maxCup = maxCup;
+    this.wearLevel = wearLevel;
+    this.timeouts = [];
   }
 
-  /**
-   * Запускает приготовление кофе
-   * @param {Coffee} coffee - кофе, которое требуется приготовить
-   * @returns {Promise<Coffee>} - промис, который выполнится после приготовления кофе
-   */
   createCoffee(coffee) {
-  }
+    
+    if (!coffee instanceof Coffee)
+			throw new Error('Входные данные не корректны');
+
+    if (this.maxCup > 0){
+      this.timeouts.push(coffee.preparationTime);
+      this.timeouts.sort();
+      this.maxCup--;
+      this.wearLevel--;
+      
+      var promise1 = new Promise ((resolve, reject) => {
+        
+        if (this.wearLevel === 0){
+          reject(coffee);
+        }
+ 
+        setTimeout(()=>{
+          resolve(coffee);
+        },coffee.preparationTime);
+      })
+              
+      promise1.then((value) => {
+        this.maxCup++;
+        this.timeouts.splice(0, 1);
+        return value;
+      })
+      
+      promise1.catch((value) => {
+        return value;
+
+      })
+    }
+    else {
+      setTimeout(() => {
+        this.createCoffee(coffee)}, this.timeouts[0]
+      );
+    };
+  };
 }
 
 module.exports = { Coffee, CoffeeMachine };
+
+const machine = new CoffeeMachine(2);
+
