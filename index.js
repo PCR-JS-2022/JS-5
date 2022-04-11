f3101b2aed4f48606f5f5b2f57a7b0a14c588193
Михалев Кирилл <mihalov.k@mail.ru>
2022-04-12 01:00:07 +0500

    Первая попытка

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ class Coffee {
    * @param {number} preparationTime - время приготовления кофе
    */
   constructor(name, preparationTime) {
+    if (typeof name !== "string" && typeof preparationTime !== "number") {
+      throw new Error("Невалидные параметры")
+    }
+    this.name = name;
+    this.preparationTime = preparationTime;
+    this.createTime = new Date().getTime();
   }
 }
 
@@ -15,7 +21,14 @@ class CoffeeMachine {
    * Создаёт экзмепляр кофемашины
    * @param {number} maxCup - кол-во чашек, в которые параллельно можно готовить кофе
    */
-  constructor(maxCup) {
+  constructor(maxCup,wearLevel = 4) {
+      if (typeof maxCup !== "number" || typeof wearLevel !== "number") {
+          throw new Error("Невалидные параметры")
+      }
+      this.maxCup = maxCup;
+      this.wearLevel = wearLevel;
+      this.queue = [];
+      this.lastCup = null;
   }
 
   /**
@@ -24,7 +37,37 @@ class CoffeeMachine {
    * @returns {Promise<Coffee>} - промис, который выполнится после приготовления кофе
    */
   createCoffee(coffee) {
+    if (!coffee instanceof Coffee) {
+      throw new Error("Это не кофе")
+    }
+    if (this.queue.length < this.maxCup) {
+        this.queue.push(coffee);
+        this.lastCup = coffee;
+    }
+    else {
+        setTimeout(() => {},this.lastCup.preparationTime);
+    }
+    const promise = new Promise((resolve,reject) => {
+        if (this.wearLevel > 0) {
+        const time = this.queue.reduce((time, elem) => {
+            time += elem.preparationTime;
+            return time;
+        })
+        setTimeout(() => {
+            resolve(coffee);
+            this.queue.filter((elem => elem.createTime !== coffee.createTime));
+            this.wearLevel -= 1;
+            }, coffee.preparationTime + time);
+        }
+        else {
+            reject(coffee);
+            this.queue.filter((elem => elem.createTime !== coffee.createTime));
+        }
+    })
+    return promise;
   }
 }
+
+
 
 module.exports = { Coffee, CoffeeMachine };
