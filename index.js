329307428044638d525340694e40dd86b6e66333
Amur <amur_almukhametov@mail.ru>
2022-04-11 19:59:16 +0500

    1 попытка

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ class Coffee {
    * @param {number} preparationTime - время приготовления кофе
    */
   constructor(name, preparationTime) {
+    this.name = name;
+    this.preparationTime = preparationTime;
   }
 }
 
@@ -15,7 +17,11 @@ class CoffeeMachine {
    * Создаёт экзмепляр кофемашины
    * @param {number} maxCup - кол-во чашек, в которые параллельно можно готовить кофе
    */
-  constructor(maxCup) {
+  constructor(maxCup, wearLevel = 4) {
+    this.maxCup = maxCup;
+    this.wearLevel = wearLevel;
+    this.queue = [];
+    this.count = 0;
   }
 
   /**
@@ -24,7 +30,58 @@ class CoffeeMachine {
    * @returns {Promise<Coffee>} - промис, который выполнится после приготовления кофе
    */
   createCoffee(coffee) {
+    const coffeePromise = new Promise((resolve, reject) =>{
+      if(this.wearLevel <= 0){
+        reject(coffee);
+        return;
+      }
+
+      if(!(this.count < this.maxCup)){
+        this.queue.push({ resolve, reject, coffee });
+        return;
+      }
+
+      this.count += 1;
+      this.wearLevel -= 1;
+
+      setTimeout(() =>{
+        this.count -= 1;
+        resolve(coffee);
+      }, coffee.preparationTime);
+      });
+
+    coffeePromise.finally(()=>{
+      if(!(this.queue.length > 0) || !(this.count < this.maxCup)){
+        return;
+      }
+
+      const nextCup = this.queue.at(0);
+      this.queue.splice(0, 1);
+
+      if(this.wearLevel <= 0){
+        nextCup.reject(nextCup.coffee);
+        return;
+      }
+
+      this.count += 1;
+      this.wearLevel -= 1;
+
+      setTimeout(() => {
+        this.count -= 1;
+        nextCup.resolve(nextCup.coffee);
+      }, nextCup.coffee.preparationTime);
+    });   
+
+    return coffeePromise;
   }
 }
 
 module.exports = { Coffee, CoffeeMachine };
+
+const log = (coffee) => console.log(coffee.name);
+
+const machine = new CoffeeMachine(2);
+
+const cappuccino = new Coffee('Капучино', 6000);
+const latte = new Coffee('Латте', 1000);
+const americano = new Coffee('Американо', 3000);
