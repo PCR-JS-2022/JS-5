cd70ea7e97789589cb1f3401356770142e105c4f
device <20yaroslav0131@gmail.com>
2022-04-12 19:48:14 +0500

    Первая попытка

--- index.js
+++ index.js
@@ -1,30 +1,55 @@
 /** Класс кофе */
 class Coffee {
-  /**
-   * Создаёт экзмепляр кофе
-   * @param {string} name - название кофе
-   * @param {number} preparationTime - время приготовления кофе
-   */
-  constructor(name, preparationTime) {
-  }
+    /**
+     * Создаёт экзмепляр кофе
+     * @param {string} name - название кофе
+     * @param {number} preparationTime - время приготовления кофе
+     */
+    constructor(name, preparationTime) {
+        this.name = name;
+        this.preparationTime = preparationTime;
+    }
 }
 
 /** Класс кофемашины */
 class CoffeeMachine {
-  /**
-   * Создаёт экзмепляр кофемашины
-   * @param {number} maxCup - кол-во чашек, в которые параллельно можно готовить кофе
-   */
-  constructor(maxCup) {
-  }
+    /**
+     * Создаёт экзмепляр кофемашины
+     * @param {number} maxCup - кол-во чашек, в которые параллельно можно готовить кофе
+     */
+    constructor(maxCup, wearLevel = 4) {
+        this.maxCup = maxCup;
+        this.wearLevel = wearLevel;
+        this.queue = [];
+    }
 
-  /**
-   * Запускает приготовление кофе
-   * @param {Coffee} coffee - кофе, которое требуется приготовить
-   * @returns {Promise<Coffee>} - промис, который выполнится после приготовления кофе
-   */
-  createCoffee(coffee) {
-  }
+    /**
+     * Запускает приготовление кофе
+     * @param {Coffee} coffee - кофе, которое требуется приготовить
+     * @returns {Promise<Coffee>} - промис, который выполнится после приготовления кофе
+     */
+    createCoffee(coffee) {
+        const promise = new Promise(async (resolve, reject) => {
+            if (this.wearLevel > 0) {
+                if (this.queue.length >= this.maxCup) {
+                    await Promise.race(this.queue.map(p => p.promise))
+                }
+
+                setTimeout(() => {
+                    this.queue.shift();
+                    resolve(coffee);
+                }, coffee.preparationTime);
+            }
+            else {
+                reject(coffee);
+                return;
+            }
+        })
+
+        this.wearLevel -= 1;
+        this.queue.push({ promise });
+        return promise;
+    }
 }
 
 module.exports = { Coffee, CoffeeMachine };
