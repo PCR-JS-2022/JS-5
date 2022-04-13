fed70c837d3a3d704c8789312fa3e9ec2440cb88
danichka <daniika.ella@gmail.com>
2022-04-13 10:49:31 +0500

    ферст трай

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ class Coffee {
    * @param {number} preparationTime - время приготовления кофе
    */
   constructor(name, preparationTime) {
+    if (typeof name != "string")
+      throw new Error('Некорректное название для кофе')
+
+    if (typeof preparationTime != "number" ||
+      !Number.isInteger(preparationTime))
+      throw new Error("Неккоректное время приготовления");
+
+    this.name = name;
+    this.preparationTime = preparationTime;
   }
 }
 
@@ -15,7 +24,29 @@ class CoffeeMachine {
    * Создаёт экзмепляр кофемашины
    * @param {number} maxCup - кол-во чашек, в которые параллельно можно готовить кофе
    */
-  constructor(maxCup) {
+  constructor(maxCup, wearLevel = 4) {
+    if (typeof maxCup != "number" || !Number.isInteger(maxCup))
+       throw new Error("Некорректное количество чашек");
+
+     if (typeof wearLevel != "number")
+       throw new Error("Некорректный уровень износа");
+
+     this.maxCup = maxCup;
+     this.wearLevel = wearLevel;
+     this.queue = [];
+   }
+
+   hasQueue() {
+     return this.queue.length >= this.maxCup;
+   }
+
+   isBroken() {
+     return this.wearLevel <= 0;
+   }
+
+   isCoffee(coffee) {
+     if (!coffee instanceof Coffee)
+       throw new Error("Не задан объект кофе");
   }
 
   /**
@@ -24,6 +55,27 @@ class CoffeeMachine {
    * @returns {Promise<Coffee>} - промис, который выполнится после приготовления кофе
    */
   createCoffee(coffee) {
+    this.isCoffee(coffee);
+
+     const promise = new Promise((resolve, reject) => {
+       if (this.isBroken()) {
+         reject(coffee);
+         return;
+       }
+
+       if (this.hasQueue()) {
+        await Promise.race(this.queue);
+        this.queue.shift();
+      }
+
+      setTimeout(() => {
+        resolve(coffee);
+      }, coffee.preparationTime);
+    });
+
+    this.wearLevel -= 1;
+    this.queue.push(promise);
+    return promise;
   }
 }
 
